4f8638991df6ce1b3dc3eb271b7e015c21916433
Dokyun Lim <im.koremp@gmail.com>
2022-07-08 19:32:13 +0900

    test: Add given2 and jest-plugin-context to jest config

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,19 @@ const nextJest = require('next/jest')
 const createJestConfig = nextJest({ dir: './' })
 
 const customJestConfig = {
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  setupFilesAfterEnv: [
+    '<rootDir>/jest.setup.js',
+    'given2/setup',
+    'jest-plugin-context/setup'
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100,
+    },
+  },
 }
 
 module.exports = createJestConfig(customJestConfig)
