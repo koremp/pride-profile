c911957465a6534a7f2f266afad230210cbfd0a2
Dokyun Lim <im.koremp@gmail.com>
2022-07-08 19:38:32 +0900

    test: Add pages basic test

---
+++ __tests__/index.test.js
@@ -0,0 +1,7 @@
+import { render } from '@testing-library/react';
+
+import IndexPage from '/pages/index'
+
+test('IndexPage', () => {
+  render(<IndexPage />)
+})
---
+++ __tests__/login.test.js
@@ -0,0 +1,7 @@
+import { render } from '@testing-library/react';
+
+import LoginPage from '/pages/login'
+
+test('LoginPage', () => {
+  render(<LoginPage />)
+})
