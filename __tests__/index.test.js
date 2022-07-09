189a5ff59262b7268adbbf4411becaf1d7fb7afd
Dokyun Lim <im.koremp@gmail.com>
2022-07-09 20:57:46 +0900

    test: update index page test

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -2,6 +2,28 @@ import { render } from '@testing-library/react'
 
 import IndexPage from '@/pages/index'
 
-test('IndexPage', () => {
-  render(<IndexPage />)
+describe('IndexPage', () => {
+
+  function renderIndexPage() {
+    return render(<IndexPage />)
+  }
+
+  it('renders title and text', () => {
+    const { getByText } = renderIndexPage()
+
+    expect(getByText('Pride Profile')).not.toBeNull()
+    expect(getByText('Create Your Own Pride Profile')).not.toBeNull()
+  })
+
+  it('renders "Sign in" button', () => {
+    const { getByRole } = renderIndexPage()
+
+    expect(getByRole('button').innerHTML).toContain('Sign In');
+  })
+
+  it('renders "Create Profile" button', () => {
+    const { getByRole } = renderIndexPage()
+
+    expect(getByRole('button').innerHTML).toContain('Create Profile');
+  })
 })
