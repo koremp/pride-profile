5e1f394cbb257da877c038197c9df5bbfb55f5fc
Dokyun Lim <im.koremp@gmail.com>
2022-07-20 16:47:17 +0900

    test: update jest config clear

--- jest.config.js
+++ jest.config.js
@@ -4,23 +4,25 @@ const nextJest = require('next/jest');
 const createJestConfig = nextJest({ dir: './' });
 
 const customJestConfig = {
+  testEnvironment: 'jsdom',
+  collectCoverageFrom: [
+    '**/*.{js,jsx,ts,tsx}',
+    '!**/*.d.ts',
+    '!**/node_modules/**',
+    '!**/.yarn/**',
+    '!**/.next/**',
+  ],
+  testMatch: [
+    '<rootDir>/__tests__/*.+(spec|test).(ts|js)?(x)',
+    '<rootDir>/src/**/?(*.)+(spec|test).[jt]s?(x)',
+  ],
   setupFilesAfterEnv: [
     '<rootDir>/jest.setup.js',
     'given2/setup',
     'jest-plugin-context/setup',
   ],
-  collectCoverage: true,
   // on node 14.x coverage provider v8 offers good speed and more or less good report
   coverageProvider: 'v8',
-  collectCoverageFrom: [
-    '**/*.{js,jsx,ts,tsx}',
-    '!**/*.d.ts',
-    '!**/node_modules/**',
-    '!<rootDir>/out/**',
-    '!<rootDir>/.next/**',
-    '!<rootDir>/*.config.js',
-    '!<rootDir>/coverage/**',
-  ],
   moduleNameMapper: {
     // Handle CSS imports (with CSS modules)
     // https://jestjs.io/docs/webpack#mocking-css-modules
@@ -34,22 +36,55 @@ const customJestConfig = {
     '^.+\\.(png|jpg|jpeg|gif|webp|avif|ico|bmp|svg)$/i': '<rootDir>/__mocks__/fileMock.js',
 
     // Handle module aliases
-    '^@/components/(.*)$': '<rootDir>/components/$1',
+    // '^@/components/(.*)$': '<rootDir>/components/$1',
     '^@/pages/(.*)$': '<rootDir>/pages/$1',
+    '^__tests__/(.*)$': '<rootDir>/__tests__/$1',
+    '^src/(.*)$': '<rootDir>/src/$1',
   },
   // Add more setup options before each test is run
   // setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/.next/'],
-  testEnvironment: 'jsdom',
   transform: {
     // Use babel-jest to transpile tests with the next/babel preset
     // eslint-disable-next-line max-len
     // https://jestjs.io/docs/configuration#transform-objectstring-pathtotransformer--pathtotransformer-object
     '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }],
   },
+  testPathIgnorePatterns: [
+    '<rootDir>/node_modules/',
+    '<rootDir>/.yarn/',
+    '<rootDir>/.next/',
+  ],
   transformIgnorePatterns: [
     '/node_modules/',
+    '/.yarn/',
+    '/.next/',
     '^.+\\.module\\.(css|sass|scss)$',
+
+    'config.js',
+    'coverage',
+
+    '_document.tsx',
+    '_app.tsx',
+    '_error.tsx',
+    '404.tsx',
+    '500.tsx',
+  ],
+  coveragePathIgnorePatterns: [
+    '/node_modules/',
+    '/.yarn/',
+    '/.next/',
+    '^.+\\.module\\.(css|sass|scss)$',
+
+    '.config.',
+    'coverage',
+    '/stories/',
+
+    '_document.tsx',
+    '_app.tsx',
+    '_error.tsx',
+    '404.tsx',
+    '500.tsx',
   ],
   coverageThreshold: {
     global: {
