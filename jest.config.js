5f87f9f7e82d0fbeca3bfa220d13f77b8edeb53f
Dokyun Lim <im.koremp@gmail.com>
2022-07-20 22:33:36 +0900

    test: add testMatch directories to jest config

--- jest.config.js
+++ jest.config.js
@@ -14,6 +14,7 @@ const customJestConfig = {
   ],
   testMatch: [
     '<rootDir>/__tests__/*.+(spec|test).(ts|js)?(x)',
+    '<rootDir>/__tests__/**/*.+(spec|test).(ts|js)?(x)',
     '<rootDir>/src/**/?(*.)+(spec|test).[jt]s?(x)',
   ],
   setupFilesAfterEnv: [
