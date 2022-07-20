e7d7a58324dbac072bac5e4b147a4f9c4386f01b
Dokyun Lim <im.koremp@gmail.com>
2022-07-20 15:59:03 +0900

    test: update jest config

--- jest.config.js
+++ jest.config.js
@@ -43,6 +43,7 @@ const customJestConfig = {
   testEnvironment: 'jsdom',
   transform: {
     // Use babel-jest to transpile tests with the next/babel preset
+    // eslint-disable-next-line max-len
     // https://jestjs.io/docs/configuration#transform-objectstring-pathtotransformer--pathtotransformer-object
     '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }],
   },
