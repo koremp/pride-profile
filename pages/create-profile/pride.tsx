e29f454b49dc603366ea04ccb0496f5e9c48cef0
Dokyun Lim <im.koremp@gmail.com>
2022-07-22 18:22:05 +0900

    feat: add mui select to pride page

--- pages/create-profile/pride.tsx
+++ pages/create-profile/pride.tsx
@@ -1,7 +1,19 @@
+import { FormControl, InputLabel, MenuItem, Select } from '@mui/material';
+
 export default function PridePage(): JSX.Element {
   return (
     <div>
-      Create Pride Profile
+      <FormControl fullWidth>
+        <InputLabel id="">Age</InputLabel>
+        <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+        >
+          <MenuItem value={10}>Ten</MenuItem>
+          <MenuItem value={20}>Twenty</MenuItem>
+          <MenuItem value={30}>Thirty</MenuItem>
+        </Select>
+      </FormControl>
     </div>
   );
 }
