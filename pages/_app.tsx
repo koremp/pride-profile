402cf213cfce88cd32a10eb08c07b50a6519c7bb
Dokyun Lim <im.koremp@gmail.com>
2022-07-22 18:18:07 +0900

    refact: update _app page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,46 @@
+import { CacheProvider, EmotionCache } from '@emotion/react';
+import { ThemeProvider } from '@mui/material/styles';
 import { Auth } from '@supabase/ui';
+import { AppProps } from 'next/app.js';
+import Head from 'next/head.js';
 import { supabase } from '../lib/initSupabase.js';
-import '../styles/index.css';
+import CssBaseline from '@mui/material/CssBaseline';
 
-export default function MyApp({ Component, pageProps }): JSX.Element {
+import theme from 'src/theme';
+import createEmotionCache from 'src/createEmotionCache';
+
+
+// Client-side cache, shared for the whole session of the user in the browser.
+const clientSideEmotionCache = createEmotionCache();
+
+interface MyAppProps extends AppProps {
+  emotionCache?: EmotionCache;
+}
+
+export default function MyApp(props: MyAppProps): JSX.Element {
+  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+
   return (
-    <Auth.UserContextProvider supabaseClient={supabase}>
-      <Component {...pageProps} />
-    </Auth.UserContextProvider>
+    <CacheProvider value={emotionCache} >
+      <Head>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+        />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/icon?family=Material+Icons"
+        />
+      </Head>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Auth.UserContextProvider supabaseClient={supabase}>
+          <Component {...pageProps} />
+        </Auth.UserContextProvider>
+      </ThemeProvider>
+
+    </CacheProvider>
+
   );
 }
