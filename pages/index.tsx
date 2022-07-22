a8cb3830b90c7a9abfd6b0718811fcef43c6ac10
Dokyun Lim <im.koremp@gmail.com>
2022-07-22 14:12:16 +0900

    feat: update index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,17 +3,13 @@ import Link from 'next/link';
 export default function IndexPage(): JSX.Element {
   return (
     <div>
-      <div>
-        <div>
-          <h1>Pride Profile</h1>
-          <Link href="/create-profile">
-            <p>create-profile</p>
-          </Link>
-          <Link href="/login">
-            <p>login</p>
-          </Link>
-        </div>
-      </div>
+      <h1>Pride Profile</h1>
+      <Link href="/create-profile">
+        <p>create-profile</p>
+      </Link>
+      <Link href="/login">
+        <p>login</p>
+      </Link>
     </div>
   );
 }
